4b8e5ae144adbcedcbd3be4142b9876eff841e62
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-05-02 17:38:19 -0400

    Created generatePartents and fitness function

--- Genetic Algorithm/knapsack.py
+++ Genetic Algorithm/knapsack.py
@@ -21,20 +21,58 @@ List of things we need:
 6. mutation function2
 '''
 
-#
-            
-
-    
+import random
 
 def main():
     n = 5 #total possible items
-    w = 10 #max weight
+    maxWeight = 10 #max weight
     names = ["pen", "laptop", "hen", "gold", "spoon"]
-    profit = [10, 40, 30, 50, 35]
-    weight = [5, 4, 6, 3, 2]
-
+    profits = [10, 40, 30, 50, 35]
+    weights = [5, 4, 6, 3, 2]
     binArr = [0,0,1,0,1] #random example
     # answer would be items [ laptop, gold, spoon ]
+
+    batch = 5 # Should be above equal to 5
+    generation = generateParents(batch,n)
+
+    print(generation)
+    fitnessScores=[]
+    for gene in generation:
+        fitnessScore = fitness(gene, weights, profits, maxWeight)
+        fitnessScores.append(fitnessScore)
+    print(fitnessScores)
     
+     
+
+#randomly generate first generation (parents)
+def generateParents(batch,n):
+    generation = []
+    generation.append([1]*n) #first parent = all 1s
+
+    for _ in range(1,batch): # 2nd parent to batch size
+        parent = [random.choice([0, 1]) for _ in range(n)]
+        generation.append(parent)
+
+    return(generation)
+        
+
+#return the fitness score for a gene (parent or child)
+def fitness(gene, weights, profits, maxWeight):
+    geneWeight = 0
+    geneProfit = 0
+    for i in range(len(gene)):
+        if gene[i]==1:
+            geneWeight += weights[i]
+            geneProfit += profits[i]
+
+    if geneWeight <= maxWeight:
+        return geneProfit
+
+    return 0
+
+
+    
+
+
 if __name__ == "__main__":
     main()
