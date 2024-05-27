7cdabb604a057e10f37520c9158daa01b1726a63
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-05-27 17:54:39 -0400

    Saved work for knapsack on genetic algo

--- Genetic Algorithm/knapsack.py
+++ Genetic Algorithm/knapsack.py
@@ -34,21 +34,27 @@ def main():
     names = ["pen", "laptop", "hen", "gold", "spoon"]
     profits = [10, 40, 30, 50, 35]
     weights = [5, 4, 6, 3, 2]
-    binArr = [0,0,1,0,1] #random example
+    # binArr = [0,0,1,0,1] #random example
     # answer would be items [ laptop, gold, spoon ]
 
     batch = 5 # Should be above equal to 5
-    generation = generateParents(batch,n)
-    fitnessScores = fitnessGeneration(generation, weights, profits, maxWeight)
-
-    print("Generation:", generation)
-    size = 0.4
-    gene1, gene2 = tournamentSelection(generation, fitnessScores, size)
-
-    print(gene1,gene2)
-
+    size = 0.4 # The percentage value of the amount genes that will be selected in tournment selection
+    generations = 2 # number of generations to be developed
     
-     
+    
+    generation = generateParents(batch, n)
+    for generationNumber in range(generations):
+        print(f"GENERATION: {generationNumber}") 
+        print(f"GENERATION Index: {generation}")   
+        fitnessScores = fitnessGeneration(generation, weights, profits, maxWeight)
+        newGeneration = crossOverGeneration(generation, fitnessScores, size, n)
+        generation = newGeneration
+
+    print(generations, " generations have been completed")
+    print("This is the last Generation' ", newGeneration)
+    bestGene = bestGeneInGeneration(generation, weights, profits, maxWeight)
+    print("BestGene:",bestGene)
+
 
 #randomly generate first generation (parents)
 def generateParents(batch,n):
@@ -61,6 +67,38 @@ def generateParents(batch,n):
 
     return generation
         
+def fitnessGeneration(generation, weights, profits, maxWeight):
+    fitnessScores = []
+    for gene in generation:
+        fitnessScore = fitness(gene, weights, profits, maxWeight)
+        fitnessScores.append(fitnessScore)
+    return fitnessScores
+        
+def crossOverGeneration(generation, fitnessScores, size, n):
+    newGeneration = selectTwoHigestParents(generation, fitnessScores, n)
+    for i in range(n-2):
+        a = i + 1
+        gene1, gene2 = tournamentSelection(generation, fitnessScores, size)
+        newChild = crossover(gene1, gene2, n)
+        newGeneration.append(newChild)
+    print(f"NEW GENERATION: {newGeneration}")
+    return newGeneration
+
+def bestGeneInGeneration(generation, weights, profits, maxWeight):
+    bestGene = generation[0]
+    bestProfit = fitness(generation[0], weights, profits, maxWeight)
+
+    for i in range(1,len(generation)):
+        gene = generation[i]
+        geneProfit = fitness(generation[i], weights, profits, maxWeight)
+        if geneProfit > bestProfit:
+            bestGene = gene
+            bestProfit = geneProfit   
+    return bestGene
+        
+
+
+# Helper Function that won't be called in main
 
 #return the fitness score for a gene (parent or child)
 def fitness(gene, weights, profits, maxWeight):
@@ -76,70 +114,6 @@ def fitness(gene, weights, profits, maxWeight):
 
     return 0
 
-def fitnessGeneration(generation, weights, profits, maxWeight):
-    fitnessScores = []
-    for gene in generation:
-        fitnessScore = fitness(gene, weights, profits, maxWeight)
-        fitnessScores.append(fitnessScore)
-    return fitnessScores
-
-
-def selectTwoHigestParenst(generation, fitnessScores, n):
-    newGeneration = []
-
-    highestGeneIndex, SecondHighestGeneIndex = findTwoHighestIndices(fitnessScores,n)
-    newGeneration.extend([generation[highestGeneIndex], generation[SecondHighestGeneIndex]])
-    return newGeneration
-
-def tournamentSelection(generation, fitnessScores, size):
-    tournamentSize = int(len(fitnessScores) * size)
-    print(tournamentSize)
-    parents = []
-    iterations = 2
-    while iterations > 0:
-        tournamentMembers = []
-        for _ in range(tournamentSize):
-            member = random.randint(0,len(fitnessScores)-1)
-            tournamentMembers.append(member) #tournamentMembers = array of indices
-        print(f"TOURNAMENT MEMBER INDICES: {tournamentMembers}")
-        highScore = 0
-        remove = -1
-        for i in tournamentMembers:
-            print(f"MEMBER {i}: {generation[i]}, {fitnessScores[i]}")
-            if fitnessScores[i] >= highScore:
-                remove = i
-                highScore = fitnessScores[i]
-        print(f"TOURNAMENT WINNER INDEX: {remove}")
-        parents.append(generation[remove])
-        del generation[remove]
-        del fitnessScores[remove]
-        iterations -= 1
-        
-    return parents[0], parents[1]
-        
-    
-    
-
-def crossover(gene1, gene2, n):
-    spliceIndex = random.randint(2,n-2)
-    bool = random.choice([True, False])
-    newChild = []
-    if bool:
-        newChild = gene1[:spliceIndex] + gene2[spliceIndex:]
-    else:
-        newChild = gene2[:spliceIndex] + gene1[spliceIndex:]
-    return newChild
-
-def crossOverGeneration(generation, fitnessScores, n):
-    newGeneration = selectTwoHigestParenst(generation, fitnessScores, n)
-    size = 0
-    for i in range(n-2):
-        a = i + 1
-        gene1, gene2 = tournamentSelection(generation, fitnessScores, size)
-        
-
-
-
 def findTwoHighestIndices(fitnessScores,n):
     index1, index2 = (0, 1) if fitnessScores[0] > fitnessScores[1] else (1, 0)
     
@@ -152,31 +126,58 @@ def findTwoHighestIndices(fitnessScores,n):
 
     return index1, index2
 
-    
+def selectTwoHigestParents(generation, fitnessScores, n):
+    newGeneration = []
 
+    highestGeneIndex, SecondHighestGeneIndex = findTwoHighestIndices(fitnessScores,n)
+    newGeneration.extend([generation[highestGeneIndex], generation[SecondHighestGeneIndex]])
+    return newGeneration
+
+def crossover(gene1, gene2, n):
+    spliceIndex = random.randint(2,n-2)
+    bool = random.choice([True, False])
+    newChild = []
+    if bool:
+        newChild = gene1[:spliceIndex] + gene2[spliceIndex:]
+    else:
+        newChild = gene2[:spliceIndex] + gene1[spliceIndex:]
+    print(f"NEW CHILD: {newChild}")
+    return newChild
+
+def tournamentSelection(generation, fitnessScores, size):
+    tournamentSize = int(len(fitnessScores) * size)
+    # print(tournamentSize)
+    parents = []
+    iterations = 2
+    while iterations > 0:
+        tournamentMembers = set()
+        while len(tournamentMembers) < tournamentSize:
+            member = random.randint(0,len(fitnessScores)-1)
+            if member not in tournamentMembers:
+                tournamentMembers.add(member) #tournamentMembers = array of indices
+                
+        # print(f"TOURNAMENT MEMBER: {tournamentMembers}")
+        highScore = 0
+        remove = -1
+        for i in tournamentMembers:
+            # print(f"MEMBER {i}: {generation[i]}, {fitnessScores[i]}")
+            if fitnessScores[i] >= highScore:
+                remove = i
+                highScore = fitnessScores[i]
+        # print(f"TOURNAMENT WINNER: {remove}")
+        parents.append(generation[remove])
+        del generation[remove]
+        del fitnessScores[remove]
+        iterations -= 1
+        
+    # print("torumnet done")
+    return parents[0], parents[1]
 
 if __name__ == "__main__":
     main()
 
 
 '''
-    newGeneration = []
-    newGeneration.extend(newGenerationUnComplete)
-    for i in range(n-2):
-        a = i + 1
-        c1, c2 = generation[i], generation[a] #c1, c2 = tournamentSelection(generation)
-        spliceIndex = random.randint(2,n-2)
-        bool = random.choice([True, False])
-        newChild = []
-        if bool:
-            newChild = c2[:spliceIndex] + c1[spliceIndex:]
-        else:
-            newChild = c1[:spliceIndex] + c2[spliceIndex:]
-        newGeneration.append(newChild)
-
-    return newGeneration
-    
-
     
     n = 5
     Highest 2 goes to the next generation
